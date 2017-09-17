922abaaa26deae914e2ad61557509c10bc39b778
Alejandra Ugarte <Alejandra_Ugarte@outlook.com>
2017-09-17 11:36:49 +0100

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,9 @@
+#Read in text file for desired dates (01-02-2007 and 02-02-2007), it needs to be separated by ";"
+#Convert to date format
+#par to be able to plot 4 graphs
+#plot each of the 4 graphs
+#save to png
+
 power <- read.table("household_power_consumption.txt",sep = ";", header = FALSE,na.strings = "?",skip = 66637,nrows = 2880)
 names(power) <- c("Date", "Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 power$Date <- as.Date(power$Date, format="%d/%m/%Y")
